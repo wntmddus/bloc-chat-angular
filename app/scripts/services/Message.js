d09bb19d0c9836703d8e5a64fa128bc78b4f979d
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-12-12 02:21:09 -0800

    assignment 5

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,21 +1,22 @@
 (function() {
-  function Message($firebaseArray) {
+  function Message($firebaseArray, $cookies) {
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
       
     return {
-      send: function (newMessage, roomId) {
+        send: function (newMessage, roomId) {
         // Filter the messages by their room ID.
-          var message = {
-              content: newMessage,
-              roomID: roomId
-          }
-          messageRef.$add(message);
-      }
+            var message = {
+                username: $cookies.get('blocChatCurrentUser'),
+                content: newMessage,
+                roomID: roomId
+            }
+            messageRef.$add(message);
+        }
     };
   }
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message]);
+    .factory('Message', ['$firebaseArray', '$cookies', Message]);
 })();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -14,10 +14,22 @@
                 templateUrl: '/templates/room.html'
             });
     }
-
+    
+    function BlocChatCookies($cookies, $uibModal) {
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        if (!currentUser || currentUser === '') {
+            $uibModal.open({
+            // Modal configuration object properties
+                templateUrl: '/templates/usernameModal.html',
+                controller: 'ModalInstanceCtrl',
+                size: 'md'
+            });
+        }
+    }
 
 
     angular
-        .module('blocChat', ['ui.bootstrap', 'ui.router', 'firebase'])
-        .config(config);
+        .module('blocChat', ['ui.bootstrap', 'ngCookies', 'ui.router', 'firebase'])
+        .config(config)
+        .run(['$cookies', '$uibModal', BlocChatCookies]);
 })();
